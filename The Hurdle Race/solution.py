8b0211bca27dce3e27be9451e48ef2681ed9358d
Raamkishore <raamkishore.p@gmail.com>
2020-07-08 21:54:46 +0530

    initial commit

---
+++ The Hurdle Race/solution.py
@@ -0,0 +1,24 @@
+def hurdleRace(k, height):
+    temp = max(height) - k
+
+    if temp > 0:
+        return temp
+    
+    return 0 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    result = hurdleRace(k, height)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
